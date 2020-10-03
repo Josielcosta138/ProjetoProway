8c3117046fb8564df0964f33aef9c4ed888ccdb0
Josielcosta138 <cjosiel2@gmail.com>
2020-10-03 15:28:23 -0300

    Testes 2 de Verificação. Ok

--- src/TebelaJogo.java
+++ src/TebelaJogo.java
@@ -1,8 +1,8 @@
-import java.sql.Connection;
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import javax.xml.transform.ErrorListener;
+
 
 /**
  * Classe Onde sera inserido Todos dados do Jogo
@@ -10,13 +10,12 @@ import javax.xml.transform.ErrorListener;
 public class TebelaJogo {
 
     //private static int placarTest;
-    private static int minTemporadaTest;
-    private static int maxTemporadaTest;
-    private int recordeminTest;
-    private int recordemaxTest;
+   // private static int minTemporadaTest;
+   // private static int maxTemporadaTest;
+   // private int recordeminTest;
+   // private int recordemaxTest;
 
 
-    String nome ;
 
     /**
      * Parametros que define a classe onde irá Executar o Programa.
@@ -45,15 +44,18 @@ public class TebelaJogo {
 
         System.out.println();
         System.out.println();
-        System.out.println("|_______________________|");
-        System.out.println("|     INICIO TABELA     |");
-        System.out.println("|     INSERIR DADOS     |");
-        System.out.println("|                       |");
-        System.out.println("| OBS:Placar numeros    |");
-        System.out.println("| Inteiros,Positivos e  |");
-        System.out.println("| Menores que 1000.     |");
-        System.out.println("|                       |");
-        System.out.println("|_______________________|");
+        System.out.println("   |______________________________|");
+        System.out.println("   |        INICIO DA TABELA      |");
+        System.out.println("   |                              |");
+        System.out.println("   |       INSERIRA OS DADOS      |");
+        System.out.println("   |                              |");
+        System.out.println("   | OBS:Placar numeros somente   |");
+        System.out.println("   |    Inteiros,Positivos e      |");
+        System.out.println("   |      Menores que 1000.       |");
+        System.out.println("   |                              |");
+        System.out.println("   |______________________________|");
+        System.out.println();
+        System.out.println();
         System.out.println();
 
 
@@ -69,25 +71,24 @@ public class TebelaJogo {
              */
             try {
 
-                System.out.println("|________________________|");
-                System.out.println("|                        |");
-                System.out.println("|        MENU JOGO       |");
-                System.out.println("|     1 INSERIR JOGO     |");
-                System.out.println("|     2 CONSULTAR TABELA |");
-                System.out.println("|     0 SAIR             |");
-                System.out.println("|________________________|");
+                System.out.println("    |________________________|");
+                System.out.println("    |                        |");
+                System.out.println("    |        MENU JOGO       |");
+                System.out.println("    |     1 INSERIR JOGO     |");
+                System.out.println("    |     2 CONSULTAR TABELA |");
+                System.out.println("    |     0 SAIR             |");
+                System.out.println("    |________________________|");
                 System.out.println();
 
                 opcao = (leiaOpcao.nextInt());
+                System.out.println();
 
             } catch (Exception erro) {
                 System.out.println("|___________________________________________|");
-                System.out.println("|                                           |");
+                System.out.println("|                  ERRO!                    |");
                 System.out.println("| Atenção! Insira somente valor *Numérico* >|" + erro);
                 System.out.println("|      Execute o código Novamente          >|" + erro);
                 System.out.println("|___________________________________________|");
-
-                opcao = (leiaOpcao.nextInt());
                 break;
             }
 
@@ -96,9 +97,7 @@ public class TebelaJogo {
              */
             switch (opcao) {
 
-                /**
-                 * Informando quantos jogos ira adicionar na tabela
-                 */
+                
                 case 1: {
                     int n;
 
@@ -106,32 +105,36 @@ public class TebelaJogo {
                      * Inicio de tratamento de erro.
                      */
                     try {
-                        System.out.println("| QUANTOS JOGOS? |");
+                        System.out.println("    | INFORME O  N° do Jogo, ? |");
+                        System.out.println("    | Ex.   1,2,3,4            |");
                         n = (leia.nextInt());
+                        System.out.println();
 
                     } catch (InputMismatchException erro) {
                         System.out.println("|___________________________________________|");
-                        System.out.println("|                                           |");
+                        System.out.println("|                  ERRO!                    |");
                         System.out.println("| Atenção! Insira somente valor *Numérico* >|" + erro);
                         System.out.println("|      Execute o código Novamente          >|" + erro);
                         System.out.println("|___________________________________________|");
                         n = (leia.nextInt());
+                        System.out.println();
                         break;
                     }
 
                     for (int i = 0; i < n; i++) {
 
                         // armazena cada Jogo
-                        System.out.println("|  Jogo :|" + (i + 1));
-                        System.out.println("| Informe o Placar:|");
+                       // System.out.println("|  Jogo :|" + (i + 1));
+                        System.out.println("    | Informe o Placar:|");
                         int placar = (leia.nextInt());
+                        System.out.println();
 
                         // Recebendo = Placar Mínimo e Máximo da temporada
 
                        
                         int recordmin = 0;
                         int recordmax = 0;
-
+ 
                         /**
                          * @while Verificações de placar e recordes. Verificação de numeros positivos e
                          *        menores que 1000.
@@ -143,8 +146,6 @@ public class TebelaJogo {
                             placar = (leia.nextInt());
                            // minTemporada = placar;
                         }
-
-                        System.out.println("| PLACAR TEST  : "+placarTest+" |");
 
                             //NOVOplacar     placar1
                         if (placar < placarTest  ){
@@ -167,8 +168,7 @@ public class TebelaJogo {
                         }
 
                         /**
-                         * @if1
-                         *  Verificações de placare recordes.
+                         * @if
                          *  Verificação para anotar o recorde.
                          */
 
@@ -184,12 +184,18 @@ public class TebelaJogo {
                             recordmin = 0;
                         }
                         
-                        
+                        if (placar  ==  placarTest){
+                            recordmax = 1;
+                            recordmin = 1;
+
+                        } 
 
                     }
                     
                         
-                        System.out.println("| Placar salvo com sucesso!  |");
+                        System.out.println("    |                          Placar salvo com sucesso!                        |");
+                        System.out.println();
+                        System.out.println("    | ATENÇÃO: Assim que informar o placar, deve-se fazer a Consulta na tabela! |");
                         System.out.println();
 
                         /**
@@ -213,25 +219,27 @@ public class TebelaJogo {
 
                         //TABELA DO JOGO
                         System.out.println();
-                        System.out.println("|__________________|");
-                        System.out.println("| Tabela Proway!   |");
-                        System.out.println("| Jogo         : "+( i+ 1 ) +" |");
-                        System.out.println("|                  |");
-                        System.out.println("| Placar       : "+ListaJogo.get(i).getPlacar()+" |");
-                        System.out.println("| Min-Temporada: "+ ListaJogo.get(i).getMinTemporada()+" |");
-                        System.out.println("| Máx-Temporada: "+ ListaJogo.get(i).getMaxTemporada()+" |");
-                        System.out.println("| Record-Min   : "+ ListaJogo.get(i).getRecordemin()+" |");
-                        System.out.println("| Record-Max   : "+ ListaJogo.get(i).getRecordemax()+" |");
-                        System.out.println("|__________________|");
+                        System.out.println("          |________________________________|");
+                        System.out.println("          |     Tabela Proway!   ");
+                        System.out.println("          |      Jogo         :    "+( i+ 1 ) );
+                        System.out.println("          |                  ");
+                        System.out.println("          |      Placar       :    "+ListaJogo.get(i).getPlacar());
+                        System.out.println("          |      Min-Temporada:    "+ ListaJogo.get(i).getMinTemporada());
+                        System.out.println("          |      Máx-Temporada:    "+ ListaJogo.get(i).getMaxTemporada());
+                        System.out.println("          |      Record-Min   :    "+ ListaJogo.get(i).getRecordemin());
+                        System.out.println("          |      Record-Max   :    "+ ListaJogo.get(i).getRecordemax());
+                        System.out.println("          |      Qntd de Jogos:    "+ListaJogo.size());
+                        System.out.println("          |________________________________|");
                         System.out.println();
-                        
-                        // Tamanho da Lista
-                        System.out.println("Qntd na Lista: "+ListaJogo.size());
+                        System.out.println();
+
+                        System.out.println("          | Insira novo Jogo ou Encerre Tabela! |");
+                        System.out.println();
                         System.out.println();
 
                         placarTest = ListaJogo.get(i).getPlacar();
-                        minTemporadaTest = ListaJogo.get(i).getMinTemporada();
-                        maxTemporadaTest = ListaJogo.get(i).getMaxTemporada();
+                       // minTemporadaTest = ListaJogo.get(i).getMinTemporada();
+                      // maxTemporadaTest = ListaJogo.get(i).getMaxTemporada();
 
                     }
                     break;
@@ -252,13 +260,7 @@ public class TebelaJogo {
         }
     }
 
-    public String getNome() {
-        return this.nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    
 
 }
 
